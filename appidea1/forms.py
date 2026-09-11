e8903290b0b07443228af3299d746fc5cf75f485
Sammy Griffiths <119477565+Sammy-Griffiths@users.noreply.github.com>
2026-09-11 14:38:09 -0230

    Wire AddProductForm widgets for number inputs and selects

--- appidea1/forms.py
+++ appidea1/forms.py
@@ -1,7 +1,45 @@
 from django import forms
 from .models import AddProduct
 
+
 class AddProductForm(forms.ModelForm):
     class Meta:
         model = AddProduct
-        fields = ['product_name','product_location', 'volume_amount', 'volume_unit', 'density']
+        fields = [
+            'product_name',
+            'product_location',
+            'volume_amount',
+            'volume_unit',
+            'density',
+        ]
+        widgets = {
+            'product_name': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'maxlength': '30',
+                    'placeholder': 'Product name',
+                }
+            ),
+            'product_location': forms.Select(
+                attrs={'class': 'form-control'}
+            ),
+            'volume_amount': forms.NumberInput(
+                attrs={
+                    'class': 'form-control',
+                    'step': '0.01',
+                    'min': '0',
+                    'placeholder': '1280.00',
+                }
+            ),
+            'volume_unit': forms.Select(
+                attrs={'class': 'form-control'}
+            ),
+            'density': forms.NumberInput(
+                attrs={
+                    'class': 'form-control',
+                    'step': '0.01',
+                    'min': '0',
+                    'placeholder': '1.25',
+                }
+            ),
+        }
